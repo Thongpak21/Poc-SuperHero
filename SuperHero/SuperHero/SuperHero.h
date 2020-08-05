2dcdbcae50a86c417669c624ed8bb6ff7fd09ec1
Thongpak Pongsilathong <thongpak.pon@truedigital.com>
2020-08-05 09:37:16 +0700

    update

---
+++ SuperHero/SuperHero/SuperHero.h
@@ -0,0 +1,19 @@
+//
+//  SuperHero.h
+//  SuperHero
+//
+//  Created by Thongpak Pongsilathong on 29/7/2563 BE.
+//  Copyright © 2563 Thongpak Pongsilathong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SuperHero.
+FOUNDATION_EXPORT double SuperHeroVersionNumber;
+
+//! Project version string for SuperHero.
+FOUNDATION_EXPORT const unsigned char SuperHeroVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperHero/PublicHeader.h>
+
+
---
+++ SuperHero/SuperHero/SuperHero.swift
@@ -0,0 +1,9 @@
+//
+//  SuperHero.swift
+//  SuperHero
+//
+//  Created by Thongpak Pongsilathong on 29/7/2563 BE.
+//  Copyright © 2563 Thongpak Pongsilathong. All rights reserved.
+//
+
+import Foundation
